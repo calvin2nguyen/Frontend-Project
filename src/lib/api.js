aa22e3afe78966b5201f6f415902234bf22a4b55
cnguyen-dev <cnguyen-dev@cnguyen-dev.myguest.virtualbox.org>
2026-05-02 02:02:28 -0700

    remove console log token

--- src/lib/api.js
+++ src/lib/api.js
@@ -2,7 +2,6 @@ import axios from "axios"
 
 //Expires every hour when generated.
 const token = import.meta.env.VITE_SPOTIFY_ACCESS_TOKEN
-console.log(token)
 export const musicApi = axios.create(  
     {
     baseURL: "https://api.spotify.com/v1/",
